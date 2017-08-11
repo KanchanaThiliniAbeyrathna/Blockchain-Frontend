ecb2b6318301e4b12a690e33b37fa7851a62589e
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2017-08-11 23:47:48 +0530

    adding project class

---
+++ src/app/models/project.ts
@@ -0,0 +1,16 @@
+/**
+ * Project
+ */
+export class Project {
+	public client: string;
+    public projectName: string;
+    public description :string;
+	public files :any ;
+    public skills: any;
+	public time_period_months : number ;
+	public time_period_weeks : number ;    
+    public budget_min : number;
+    public budget_max : number;
+    public budget_type : string;
+    
+}
