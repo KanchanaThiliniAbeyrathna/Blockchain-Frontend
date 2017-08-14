420ac9a450c979e0ac603c0e6f772eb40e0299c9
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2017-08-14 18:16:30 +0530

    added post/view project functions

--- src/app/components/work/project-detail.component.ts
+++ src/app/components/work/project-detail.component.ts
@@ -1,20 +1,49 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { MyService } from  "../../services/service";
-import { Router} from '@angular/router';
+import { MyService } from "../../services/service";
+import { Router, Params, ActivatedRoute } from '@angular/router';
 declare var require: any;
+
+import { Project } from "../../models/project";
 
 @Component({
     selector: 'my-project-detail',
     templateUrl: './project-detail.component.html',
-    providers :[MyService]
+    providers: [MyService]
 })
 export class ProjectDetailComponent implements OnInit {
 
-    constructor(private _router: Router ,private _service: MyService) {
-        
+    project: Project;
+    projctsStream: string = "projects";
+
+    constructor(private _service: MyService, private _route: ActivatedRoute, private _router: Router) {
+
     }
 
-    ngOnInit() { 
+    ngOnInit() {
+        this._route.params.forEach((params: Params) => {
+            if (params['project_id'] !== undefined) {
+                let project_id = params['project_id'];
+                this._service.getstreamitem(this.projctsStream, project_id.toString())
+                    .then(data => {
+                        this.project = JSON.parse(this.Hex2String(data.data.toString()));
+                        this.project.project_id = data.txid;
+                        this.project.client = data.publishers[0];
+                    });
+            } else {
+
+            }
+        });
+    }
+
+    Hex2String(hex_str: string) {
+        let j;
+        let hexes = hex_str.match(/.{1,4}/g) || [];
+        let result_back = "";
+        for (j = 0; j < hexes.length; j++) {
+            result_back += String.fromCharCode(parseInt(hexes[j], 16));
+        }
+
+        return result_back;
     }
 
 }
--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,16 +1,17 @@
 /**
  * Project
  */
+import {TimePeriod} from "./timeperiod";
+import {Budget} from "./budget";
+
 export class Project {
+    public project_id : string;
 	public client: string;
     public projectName: string;
     public description :string;
-	public files :any ;
-    public skills: any;
-	public time_period_months : number ;
-	public time_period_weeks : number ;    
-    public budget_min : number;
-    public budget_max : number;
-    public budget_type : string;
+	public files :string[] ;
+    // public skills: any;
+	public time_period: TimePeriod;
+    public budget: Budget;
     
 }
